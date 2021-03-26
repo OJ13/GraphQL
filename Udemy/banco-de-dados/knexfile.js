e27428811cf5dc1c798130eeb1d5b2f7d25dc7ca
Osmar Junior <oj13black@hotmail.com>
2021-03-26 08:33:11 -0300

    Add knex e config

---
+++ Udemy/banco-de-dados/knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+const { connection } = require('./.env')
+
+module.exports = {
+  production: {
+    client: 'mysql',
+    connection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};

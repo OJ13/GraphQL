466d39b9f073589ae5f9a447e6c83775b109ae3c
Osmar Junior <oj13black@hotmail.com>
2021-04-05 09:00:39 -0300

    add migration perfil e usuarios

--- Udemy/banco-de-dados/projeto/migrations/20210326115844_tabela_perfis.js
+++ Udemy/banco-de-dados/projeto/migrations/20210326115844_tabela_perfis.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('perfis', table => {
+      table.increments('id').primary()
+      table.string('nome').notNull().unique()
+      table.string('rotulo').notNull()
+  }).then(function() {
+      return knex('perfis').insert([
+          { nome: 'admin', rotulo: 'Administrador' },
+          { nome: 'master', rotulo: 'Master' },
+          { nome: 'usuario', rotulo: 'Usuario' }
+      ])
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('perfis')
 };

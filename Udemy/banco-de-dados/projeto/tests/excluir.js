24fcfc44e5d4e6b05f25b147a2ccdbb22219c117
Osmar Junior <oj13black@hotmail.com>
2021-04-19 09:25:09 -0300

    delete js

--- Udemy/banco-de-dados/projeto/tests/excluir.js
+++ Udemy/banco-de-dados/projeto/tests/excluir.js
@@ -1,4 +1,13 @@
-const db = requeire('../config/db')
+const db = require('../config/db')
 
 //excluir por Id
-db('usuarios')
+// db('usuarios').where({ id: 1})
+//     .delete()
+//     .then(res => console.log(res))
+//     .finally(() => db.destroy())
+
+//todos os perfis
+db('perfis')
+    .delete()
+    .then(res => console.log(res))
+    .finally(() => db.destroy())

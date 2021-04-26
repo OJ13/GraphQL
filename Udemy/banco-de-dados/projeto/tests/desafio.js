d424094f3184902a7dd4781be17eb211f9d935df
Osmar Junior <oj13black@hotmail.com>
2021-04-26 09:33:40 -0300

    final desafio

--- Udemy/banco-de-dados/projeto/tests/desafio.js
+++ Udemy/banco-de-dados/projeto/tests/desafio.js
@@ -2,57 +2,78 @@ const db = require('../config/db')
 
 async function salvarUsuario(nome, email, senha) {
     const verificarUsuario = await db('usuarios').where({ email: email }).first()
-    
+    let user    
     if (verificarUsuario) {
-        await db('usuarios').where({ id: verificarUsuario.id })
+        user = await db('usuarios').where({ id: verificarUsuario.id })
                     .update({ nome : nome, email : email, senha : senha })
                     .then(() => console.log(`Usuário  do id: ${verificarUsuario.id} alterado com Sucesso`))
                     .catch(err => console.log(err.sqlMessage))
+
+        user = await db('usuarios').where({ id: verificarUsuario.id }).first();
     } else {
         const novoUsuario = {
             nome: nome, email: email, senha: senha
         }
-        await db('usuarios').insert(novoUsuario)
-            .then('Novo usuário criado com Sucesso')
-            .catch(err => console.log(err.sqlMessage))
+        user = await db('usuarios').insert(novoUsuario)
+                                    .then((res) => res)
+                                    .then('Novo usuário criado com Sucesso')
+                                    .catch(err => console.log(err.sqlMessage))
     }
+    return user;
 }
 
 async function salvarPerfil(nome, rotulo) {
-    const verificarPerfil = await db('perfil').orWhere({ nome : nome }).orWhere({ rotulo : rotulo }).first()
-
+    const verificarPerfil = await db('perfis').orWhere({ nome : nome }).orWhere({ rotulo : rotulo }).first()
+    let perfil
     if (verificarPerfil) {
-        await db('pefil').where({ id: verificarPerfil.id })
+        await db('perfis').where({ id: verificarPerfil.id })
                 .update({
                     nome: nome, rotulo: rotulo
                 })
                 .then(() => console.log(`Perfil de id: ${verificarPerfil.id} alterado com Sucessso`))
                 .catch(err => console.log(err.sqlMessage))
+
+        perfil = await db('perfis').where({ id: verificarPerfil.id }).first()
     } else {
         const novoPerfil = { nome : nome, rotulo : rotulo };
-        await db('perfil').insert(novoPerfil)
-                .then(() => console.log('Novo perfil Criado com Sucesso'))
-                .catch(err => console.log(err.sqlMessage))
+        perfil = await db('perfis').insert(novoPerfil)
+                            .then((res) => res)
+                            .then(() => console.log('Novo perfil Criado com Sucesso'))
+                            .catch(err => console.log(err.sqlMessage))
     }
+
+    return perfil
 }
 
 async function adicionarPerfis(usuario, ...perfis) {
-
+    const verificaUsePerfil = db('usuarios_perfis').where({ usuario_id: usuario.id}).first()
+    if (verificaUsePerfil) {
+        await db('usuarios_perfis')
+                .where({ usuario_id : usuario.id })
+                .delete()
+    }
+    
+    for (perfil of perfis) {
+        await db('usuarios_perfis').insert({ usuario_id : usuario.id, perfil_id: perfil.id })
+    }
 }
 
 async function executar() {
-    const usuario1 = await salvarUsuario('Rocky Balboa', 'rocky.balboa@email.com', '123456')
-    const usuario2 = await salvarUsuario('Apollo Creed', 'apolo.creed@email.com', '123456')
-    // const usuario2 = await salvarUsuario('Adrienne', 'adrienne.balboa@email.com', '123456')
-    // const perfilA = await salvarPerfil('rh', 'Pessoal')
-    // const perfilB = await salvarPerfil('finc', 'financeiro')
+    //const usuario1 = await salvarUsuario('Rocky Balboa', 'rocky.balboa@email.com', '123456')
+    // const usuario2 = await salvarUsuario('Apollo Creed', 'apolo.creed@email.com', '123456')
+    // const usuario3 = await salvarUsuario('Adrienne', 'adrienne.balboa@email.com', '123456')
+     const usuario4 = await salvarUsuario('Adonis Creed', 'adonis.creed@email.com', '123456')
+     const perfilA = await salvarPerfil('adm', 'Administrador')
+     const perfilB = await salvarPerfil('rh', 'Pessoal')
+     const perfilC = await salvarPerfil('finc', 'financeiro')
+     const perfilD = await salvarPerfil('user', 'usuario')
 
     //  console.log(usuario1)
-    // console.log(usuario2)
-    // console.log(perfilA)
-    // console.log(perfilB)
+    console.log(usuario4)
+    console.log(perfilA)
+    console.log(perfilB)
 
-    // await adicionarPerfis(usuario2, perfilA, perfilB)
+     await adicionarPerfis(usuario4, perfilA, perfilB)
 }
 
 executar()

6ce71cbb535d86d9ff8bb4a4e37c86c72a6abed8
Osmar Junior <oj13black@hotmail.com>
2021-05-28 09:17:03 -0300

    add novo usuario, excluir usuario

--- Udemy/desafio/desafio/resolvers/Mutation/usuario.js
+++ Udemy/desafio/desafio/resolvers/Mutation/usuario.js
@@ -1,4 +1,6 @@
 const db = require('../../config/db')
+const { perfil: ObterPerfil } = require('../Query/perfil')
+const { usuario: ObterUsuario } = require('../Query/usuario')
 
 module.exports = {
     async novoUsuario(_, { dados }) {
@@ -9,34 +11,46 @@ module.exports = {
                 throw new Error("Usuário já existente")
             }
 
-            const novoUsuario = {
-                nome: dados.nome,
-                email: dados.email,
-                senha: dados.senha,
-                perfis: dados.perfis
+            const idPerfis = [];
+            if (dados.perfis) {
+                for(perfilFitro of dados.perfis) {
+                    const perfil = await ObterPerfil(_, { filtro: { ...perfilFitro } });
+
+                    if (perfil) {
+                        idPerfis.push(perfil.id);
+                    }
+                }
             }
 
-            await db('usuarios').insert(novoUsuario)
-                            .then(res => res)
-                            .catch(err => console.log(err.sqlMessage))
-                            .finally(() => db.destroy())
+            delete dados.perfis
 
+            const [id] = await db('usuarios').insert({ ...dados }).then(res => res)
+
+            for(perfil_id of idPerfis) {
+                await db('usuarios_perfis').insert({ perfil_id, usuario_id: id })
+            }
+
+            return db('usuarios').where({ id }).first();
         } catch (e) {
             console.log(e.sqlMessage)
             throw new Error("Erro ao criar Usuário")
         }
     },
     async excluirUsuario(_, { filtro }) {
-        const usuarioExistente = await db('usuarios').where({ email: filtro.email}).first();
+        try {
+            const usuarioExistente = await ObterUsuario(_, { filtro })
 
-        if (usuarioExistente) {
-            throw new Error("Usuário não encontrado para exclusão")
+            if (usuarioExistente) {
+                await db('usuarios_perfis').where({ usuario_id: usuarioExistente.id }).delete();
+                await db('usuarios').where({ id: usuarioExistente.id }).delete()
+            } else {
+                throw new Error("Usuário não encontrado para exclusão")
+            }
+            return usuarioExistente
+        } catch (e) {
+            console.log(e.sqlMessage)
+            throw new Error("Erro ao excluir Usuário")
         }
-
-        await db('usuarios').where({ id: usuarioExistente.id }).delete()
-                            .then(res => res)
-                            .catch(err => console.log(err.sqlMessage))
-
     },
     async alterarUsuario(_, { filtro, dados }) {
         const usuarioExistente = await db('usuarios').where({ email: filtro.email}).first();
